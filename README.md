d7bcb8677a516706b026a44bd862f020632a97ad
Everton <ton.oliveira@sempre.facig.edu.br>
2020-05-24 21:20:21 -0300

    Cadastra categoria

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 
 <div align="center">
-  <img src=""/>
+  <img src="https://raw.githubusercontent.com/tonoliveira96/gostack11-desafio06/master/assets/image-desafio-06.png"/>
 </div>
  <h1 align="center">Gostack11 Desafio06</h1>
 
-Desafio 02: Conceitos do Node.js
+Desafio 06: Database upload
 
 # Instalação
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,51 @@
 // import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
 
+interface Request {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
     // TODO
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getRepository(Category);
+
+    let transactionCategory = await categoryRepository.findOne({
+      where: {
+        title: category,
+      },
+    });
+
+    if (!transactionCategory) {
+      transactionCategory = categoryRepository.create({
+        title: category,
+      });
+
+      await categoryRepository.save(transactionCategory);
+    }
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+      category: transactionCategory,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
